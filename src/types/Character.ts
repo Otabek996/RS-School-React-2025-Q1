a9b6765d2f3dc98dd28e647ce9dd9903dc113ccb
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-02-05 09:30:35 +0500

    feat: update charakter types

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,7 +1,18 @@
 export interface Character {
   id: number;
   name: string;
-  image: string;
-  species: string;
   status: string;
+  species: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  created: string;
 }
