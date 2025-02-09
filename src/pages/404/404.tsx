d89b3ff7238a5accf68f16082d0aded5131ba76b
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-02-10 00:42:49 +0500

    fix: remove unnecessary styles

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -3,19 +3,21 @@ import './404.css';
 
 const NotFound: React.FC = () => {
   return (
-    <div className="background-img">
-      <div className="space"></div>
-      <div className="wrapper">
-        <div className="img-wrapper">
-          <span>44</span>
+    <div className="notFound-page">
+      <div className="background-img">
+        <div className="space"></div>
+        <div className="wrapper">
+          <div className="img-wrapper">
+            <span>44</span>
+          </div>
+          <p>
+            The page you are trying to search has been <br /> moved to another
+            universe.
+          </p>
+          <button onClick={() => (window.location.href = '/')}>
+            GET ME HOME
+          </button>
         </div>
-        <p>
-          The page you are trying to search has been <br /> moved to another
-          universe.
-        </p>
-        <button onClick={() => (window.location.href = '/')}>
-          GET ME HOME
-        </button>
       </div>
     </div>
   );
